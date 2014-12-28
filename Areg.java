bfb7af8dba85a31da84408f2d0a79e137c696160
David Hunt <huntdog1541@gmail.com>
2014-12-28 17:43:28 -0600

    Create Areg.java

---
+++ Areg.java
@@ -0,0 +1,56 @@
+
+public class Areg {
+	
+	//8-bit Lower
+	char[] AL = new char[8];
+	char[] AH = new char[8];
+	
+	char[] AX = new char[16];
+	
+	char[] EAX = new char[32];
+	
+	char[] RAX = new char[64];
+
+	
+	public char[] getAL() {
+		return AL;
+	}
+
+	public void setAL(char[] aL) {
+		AL = aL;
+	}
+
+	public char[] getAH() {
+		return AH;
+	}
+
+	public void setAH(char[] aH) {
+		AH = aH;
+	}
+
+	public char[] getAX() {
+		return AX;
+	}
+
+	public void setAX(char[] aX) {
+		AX = aX;
+	}
+
+	public char[] getEAX() {
+		return EAX;
+	}
+
+	public void setEAX(char[] eAX) {
+		EAX = eAX;
+	}
+
+	public char[] getRAX() {
+		return RAX;
+	}
+
+	public void setRAX(char[] rAX) {
+		RAX = rAX;
+	}
+	
+	
+}
