fcfadaa6b6ef28a566d18ecaea7709de2d3ea9c5
David Hunt <huntdog1541@gmail.com>
2014-12-28 17:43:12 -0600

    Create GeneralReg.java

---
+++ GeneralReg.java
@@ -0,0 +1,152 @@
+
+public class GeneralReg {
+	
+	//8-bit Lower bit
+	char[] AL   = new char[8];
+	char[] BL   = new char[8];
+	char[] CL   = new char[8];
+	char[] DL   = new char[8];
+	char[] SPL  = new char[8];
+	char[] BPL  = new char[8];
+	char[] SIL  = new char[8];
+	char[] DIL  = new char[8];
+	char[] R8B  = new char[8];
+	char[] R9B  = new char[8];
+	char[] R10B = new char[8];
+ 	char[] R11B = new char[8];
+ 	char[] R12B = new char[8];
+ 	char[] R13B = new char[8];
+ 	char[] R14B = new char[8];
+ 	char[] R15B = new char[8];
+ 	
+ 	//8-bit Higher bit
+ 	char[] AH = new char[8];
+ 	char[] BH = new char[8];
+ 	char[] CH = new char[8];
+ 	char[] DH = new char[8];
+	
+ 	
+ 	public char[] getAL() {
+		return AL;
+	}
+	public void setAL(char[] aL) {
+		AL = aL;
+	}
+	public char[] getBL() {
+		return BL;
+	}
+	public void setBL(char[] bL) {
+		BL = bL;
+	}
+	public char[] getCL() {
+		return CL;
+	}
+	public void setCL(char[] cL) {
+		CL = cL;
+	}
+	public char[] getDL() {
+		return DL;
+	}
+	public void setDL(char[] dL) {
+		DL = dL;
+	}
+	public char[] getSPL() {
+		return SPL;
+	}
+	public void setSPL(char[] sPL) {
+		SPL = sPL;
+	}
+	public char[] getBPL() {
+		return BPL;
+	}
+	public void setBPL(char[] bPL) {
+		BPL = bPL;
+	}
+	public char[] getSIL() {
+		return SIL;
+	}
+	public void setSIL(char[] sIL) {
+		SIL = sIL;
+	}
+	public char[] getDIL() {
+		return DIL;
+	}
+	public void setDIL(char[] dIL) {
+		DIL = dIL;
+	}
+	public char[] getR8B() {
+		return R8B;
+	}
+	public void setR8B(char[] r8b) {
+		R8B = r8b;
+	}
+	public char[] getR9B() {
+		return R9B;
+	}
+	public void setR9B(char[] r9b) {
+		R9B = r9b;
+	}
+	public char[] getR10B() {
+		return R10B;
+	}
+	public void setR10B(char[] r10b) {
+		R10B = r10b;
+	}
+	public char[] getR11B() {
+		return R11B;
+	}
+	public void setR11B(char[] r11b) {
+		R11B = r11b;
+	}
+	public char[] getR12B() {
+		return R12B;
+	}
+	public void setR12B(char[] r12b) {
+		R12B = r12b;
+	}
+	public char[] getR13B() {
+		return R13B;
+	}
+	public void setR13B(char[] r13b) {
+		R13B = r13b;
+	}
+	public char[] getR14B() {
+		return R14B;
+	}
+	public void setR14B(char[] r14b) {
+		R14B = r14b;
+	}
+	public char[] getR15B() {
+		return R15B;
+	}
+	public void setR15B(char[] r15b) {
+		R15B = r15b;
+	}
+	public char[] getAH() {
+		return AH;
+	}
+	public void setAH(char[] aH) {
+		AH = aH;
+	}
+	public char[] getBH() {
+		return BH;
+	}
+	public void setBH(char[] bH) {
+		BH = bH;
+	}
+	public char[] getCH() {
+		return CH;
+	}
+	public void setCH(char[] cH) {
+		CH = cH;
+	}
+	public char[] getDH() {
+		return DH;
+	}
+	public void setDH(char[] dH) {
+		DH = dH;
+	}
+
+ 	
+ 	
+}
